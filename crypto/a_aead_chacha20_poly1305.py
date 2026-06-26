2bf0c2f88b973ce204cb362a4040a49b234128fe
Amjad Adi <1230800@student.birzeit.edu>
2026-06-26 08:12:12 +0300

    Decrypt chacha20 Using the decrypt function

--- crypto/a_aead_chacha20_poly1305.py
+++ crypto/a_aead_chacha20_poly1305.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from crypto.a_poly1305_key_gen import poly1305_key_gen
-from crypto.a_chacha20 import chacha20_encrypt
+from crypto.a_chacha20 import chacha20_encrypt,chacha20_decrypt
 from crypto.a_poly1305 import poly1305_mac
 from crypto.a_poly1305 import num_to_n_le_bytes
 def pad16(x):
@@ -35,5 +35,5 @@ def chacha20_aead_decrypt(aad, key, iv, constant, ciphertext,received_tag):
     calculated_tag = poly1305_mac(mac_data, otk)
     if received_tag != calculated_tag:
         return None
-    plaintext = chacha20_encrypt(key, 1, nonce, ciphertext)
+    plaintext = chacha20_decrypt(key, 1, nonce, ciphertext)
     return plaintext
